9b4428ab5d0741e0d97217817708b5d79372c64e
Ondrej Maxa <ondrej.maxa11@gmail.com>
2026-02-14 23:40:43 +0100

    Remove build scripts from applications and inline migration runner
    Applications (bot, server) don't need codegen/build/babel scripts.
    Inline the migration run program into the migrations package index.

--- packages/migrations/src/index.ts
+++ packages/migrations/src/index.ts
@@ -3,7 +3,7 @@ import { NodeFileSystem } from "@effect/platform-node"
 import { Migrator } from "@effect/sql"
 import { fromFileSystem } from "@effect/sql/Migrator/FileSystem"
 import { PgClient } from "@effect/sql-pg"
-import { Config, Layer } from "effect"
+import { Config, Effect, Layer } from "effect"
 
 const migrationsDir = fileURLToPath(new URL("migrations", import.meta.url))
 
@@ -13,6 +13,12 @@ const PgLive = PgClient.layerConfig({
 
 const migrator = Migrator.make({})
 
-export const DatabaseLive = migrator({
+const DatabaseLive = migrator({
   loader: fromFileSystem(migrationsDir),
 }).pipe(Layer.effectDiscard, Layer.provideMerge(PgLive), Layer.provide(NodeFileSystem.layer))
+
+const program = Effect.gen(function* () {
+  yield* Effect.logInfo("Migrations completed successfully")
+})
+
+export const migrate = Effect.provide(program, DatabaseLive)
--- packages/migrations/src/run.ts
+++
@@ -1,9 +0,0 @@
-import { NodeRuntime } from "@effect/platform-node"
-import { Effect } from "effect"
-import { DatabaseLive } from "./index.js"
-
-const program = Effect.gen(function* () {
-  yield* Effect.logInfo("Migrations completed successfully")
-})
-
-program.pipe(Effect.provide(DatabaseLive), NodeRuntime.runMain)
