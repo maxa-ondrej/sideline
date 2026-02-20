695fc329514afd801643c775fcf7cd499f273925
Ondrej Maxa <ondrej.maxa11@gmail.com>
2026-02-20 02:11:47 +0100

    Add DISCORD_REDIRECT to test env config

--- applications/server/vitest.config.ts
+++ applications/server/vitest.config.ts
@@ -6,6 +6,7 @@ export default defineProject({
       DATABASE_URL: 'postgres://test:test@localhost/test',
       DISCORD_CLIENT_ID: 'test-client-id',
       DISCORD_CLIENT_SECRET: 'test-client-secret',
+      DISCORD_REDIRECT: 'http://localhost',
       FRONTEND_URL: 'http://localhost:5173',
       SERVER_URL: 'http://localhost',
     },
