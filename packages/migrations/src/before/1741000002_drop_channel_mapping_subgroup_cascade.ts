7f6cad2a344ec297c8ba7ee4d95bdfec0395743e
Ondrej Maxa <ondrej.maxa11@gmail.com>
2026-02-26 23:30:46 +0100

    Drop subgroup FK entirely so subgroup deletion is not blocked by existing channel mappings

--- packages/migrations/src/before/1741000002_drop_channel_mapping_subgroup_cascade.ts
+++ packages/migrations/src/before/1741000002_drop_channel_mapping_subgroup_cascade.ts
@@ -1,20 +1,10 @@
 import { SqlClient } from '@effect/sql';
 import { Effect } from 'effect';
 
-export default Effect.flatMap(SqlClient.SqlClient, (sql) =>
-  Effect.Do.pipe(
-    Effect.tap(
-      () => sql`
-        ALTER TABLE discord_channel_mappings
-          DROP CONSTRAINT discord_channel_mappings_subgroup_id_fkey
-      `,
-    ),
-    Effect.tap(
-      () => sql`
-        ALTER TABLE discord_channel_mappings
-          ADD CONSTRAINT discord_channel_mappings_subgroup_id_fkey
-            FOREIGN KEY (subgroup_id) REFERENCES subgroups(id)
-      `,
-    ),
-  ),
+export default Effect.flatMap(
+  SqlClient.SqlClient,
+  (sql) => sql`
+    ALTER TABLE discord_channel_mappings
+      DROP CONSTRAINT discord_channel_mappings_subgroup_id_fkey
+  `,
 );
