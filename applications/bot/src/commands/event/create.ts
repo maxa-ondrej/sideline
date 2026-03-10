bbd8779a34c418387a9782b984af42c44b10bf16
Ondrej Maxa <ondrej.maxa11@gmail.com>
2026-03-10 19:56:26 +0100

    Group /event-create and /event-list into /event subcommands

---
+++ applications/bot/src/commands/event/create.ts
@@ -0,0 +1,109 @@
+import * as m from '@sideline/i18n/messages';
+import * as Ix from 'dfx/Interactions/index';
+import { Interaction } from 'dfx/Interactions/index';
+import * as DiscordTypes from 'dfx/types';
+import { Array, Effect, Option, pipe } from 'effect';
+import { userLocale } from '~/locale.js';
+
+export const createHandler = Interaction.pipe(
+  Effect.map((interaction) => {
+    const locale = userLocale(interaction);
+    const guildId = interaction.guild_id;
+
+    if (!guildId) {
+      return Ix.response({
+        type: DiscordTypes.InteractionCallbackTypes.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: { content: m.bot_event_no_guild({}, { locale }), flags: 64 },
+      });
+    }
+
+    const data = interaction.data;
+    const eventType =
+      data && 'options' in data
+        ? pipe(
+            [...(data.options ?? [])],
+            Array.findFirst((o) => o.name === 'type'),
+            Option.flatMap((o) => ('value' in o ? Option.some(String(o.value)) : Option.none())),
+            Option.getOrElse(() => 'other'),
+          )
+        : 'other';
+
+    return Ix.response({
+      type: DiscordTypes.InteractionCallbackTypes.MODAL,
+      data: {
+        custom_id: `event-create:${eventType}`,
+        title: m.bot_event_modal_title({}, { locale }),
+        components: [
+          {
+            type: 1,
+            components: [
+              {
+                type: 4,
+                custom_id: 'event_title',
+                label: m.bot_event_title_label({}, { locale }),
+                style: 1,
+                required: true,
+                max_length: 100,
+              },
+            ],
+          },
+          {
+            type: 1,
+            components: [
+              {
+                type: 4,
+                custom_id: 'event_start',
+                label: m.bot_event_start_label({}, { locale }),
+                style: 1,
+                required: true,
+                placeholder: m.bot_event_start_placeholder({}, { locale }),
+                max_length: 16,
+              },
+            ],
+          },
+          {
+            type: 1,
+            components: [
+              {
+                type: 4,
+                custom_id: 'event_end',
+                label: m.bot_event_end_label({}, { locale }),
+                style: 1,
+                required: false,
+                placeholder: m.bot_event_end_placeholder({}, { locale }),
+                max_length: 16,
+              },
+            ],
+          },
+          {
+            type: 1,
+            components: [
+              {
+                type: 4,
+                custom_id: 'event_location',
+                label: m.bot_event_location_label({}, { locale }),
+                style: 1,
+                required: false,
+                placeholder: m.bot_event_location_placeholder({}, { locale }),
+                max_length: 200,
+              },
+            ],
+          },
+          {
+            type: 1,
+            components: [
+              {
+                type: 4,
+                custom_id: 'event_description',
+                label: m.bot_event_description_label({}, { locale }),
+                style: 2,
+                required: false,
+                max_length: 1000,
+              },
+            ],
+          },
+        ],
+      },
+    });
+  }),
+);
---
+++ applications/bot/src/commands/event/list.ts
@@ -0,0 +1,71 @@
+import { Discord } from '@sideline/domain';
+import * as m from '@sideline/i18n/messages';
+import * as Ix from 'dfx/Interactions/index';
+import { Interaction } from 'dfx/Interactions/index';
+import * as DiscordTypes from 'dfx/types';
+import { Effect } from 'effect';
+import { userLocale } from '~/locale.js';
+import { buildEventListEmbed, PAGE_SIZE } from '~/rest/events/buildEventListEmbed.js';
+import { SyncRpc } from '~/services/SyncRpc.js';
+
+export const listHandler = Interaction.pipe(
+  Effect.flatMap((interaction) => {
+    const locale = userLocale(interaction);
+    const guildId = interaction.guild_id;
+
+    if (!guildId) {
+      return Effect.succeed(
+        Ix.response({
+          type: DiscordTypes.InteractionCallbackTypes.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: { content: m.bot_event_no_guild({}, { locale }), flags: 64 },
+        }),
+      );
+    }
+
+    const snowflakeGuildId = Discord.Snowflake.make(guildId);
+
+    return Effect.Do.pipe(
+      Effect.bind('rpc', () => SyncRpc),
+      Effect.flatMap(({ rpc }) =>
+        rpc['Event/GetUpcomingGuildEvents']({
+          guild_id: snowflakeGuildId,
+          offset: 0,
+          limit: PAGE_SIZE,
+        }),
+      ),
+      Effect.map((result) => {
+        const payload = buildEventListEmbed({
+          events: result.events,
+          total: result.total,
+          offset: 0,
+          guildId,
+          locale,
+        });
+        return Ix.response({
+          type: DiscordTypes.InteractionCallbackTypes.CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            embeds: payload.embeds,
+            components: payload.components,
+            flags: 64,
+          },
+        });
+      }),
+      Effect.catchTag('GuildNotFound', () =>
+        Effect.succeed(
+          Ix.response({
+            type: DiscordTypes.InteractionCallbackTypes.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: { content: m.bot_event_not_member({}, { locale }), flags: 64 },
+          }),
+        ),
+      ),
+      Effect.catchAll(() =>
+        Effect.succeed(
+          Ix.response({
+            type: DiscordTypes.InteractionCallbackTypes.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: { content: m.bot_event_list_error({}, { locale }), flags: 64 },
+          }),
+        ),
+      ),
+    );
+  }),
+);
